33adc31a4b14c103ede69c9895f97a2c5966352f
Munir Husseini <m.husseini@gmx.de>
2016-10-05 20:17:43 +0200

    now returns rooted project paths

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,12 @@ var vsslnparse = require("vssln-parser");
 var fs = require("fs");
 var Vinyl = require("vinyl");
 var through = require('through2');
+var path = require('path');
 
 module.exports = function() {
     return through.obj(function (file, encoding, done) {
+        const root = path.dirname(file.path);
+
         var input;
         if (file.isStream()) {
             input = file.stream;
@@ -23,7 +26,8 @@ module.exports = function() {
         vsslnparse(input, function (solution) {
             const sortedProjects = dependencies.fromSolution(solution);
             sortedProjects.forEach(function (project) {
-                var file = new Vinyl({path: project.path});
+                const projectPath = path.join(root, project.path);
+                var file = new Vinyl({path: projectPath});
                 $this.push(file);
             });
             done();
--- test/tests.ts
+++ test/tests.ts
@@ -1,21 +1,22 @@
 ///<reference path="../_references.ts"/>
 
-var slnparser = require("vssln-parser");
 var vsprojects = require("../../");
 var gulp = require('gulp');
+var path = require('path');
 var tap = require('gulp-tap');
 var expect: Chai.ExpectStatic = require('chai').expect;
 
 describe('Gulp sln dependencies', function () {
     const solutionFileName = "dist/test/test.sln";
+    const root = path.join(process.cwd(), path.dirname(solutionFileName));
 
     it("projects must be returned in the correct order", done => {
         var expectedProjects = [
-            'Project4\\Project4.csproj',
-            'Project3\\Project3.csproj',
-            'Project2\\Project2.csproj',
-            'Project1\\Project1.csproj',
-            'Project5\\Project5.csproj'
+            path.join(root, 'Project4\\Project4.csproj'),
+            path.join(root, 'Project3\\Project3.csproj'),
+            path.join(root, 'Project2\\Project2.csproj'),
+            path.join(root, 'Project1\\Project1.csproj'),
+            path.join(root, 'Project5\\Project5.csproj')
         ];
         var index = 0;
 
